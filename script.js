69b15a7fb808e848771e9d04f7ed2e76c541f0e9
Theo <170449321+illtrythisout@users.noreply.github.com>
2024-09-26 22:28:09 +0200

    Completed library code

--- script.js
+++ script.js
@@ -7,59 +7,91 @@ function Book (title, author, pages, readStatus) {
     this.readStatus = readStatus;
 }
 
-function addBookToLibrary(book) {
-    myLibrary.unshift(book);
-}
+const bookshelf = document.querySelector("#bookshelf");
 
-const bookshelf = document.querySelector("#bookshelf")
-function displayBook(bookObj) {
+Book.prototype.displayBook = function() {
     const bookContainer = document.createElement("div");
     bookContainer.classList.add("bookContainer");
-
+    
     const book = document.createElement("div");
     book.classList.add("book");
+    if (this.readStatus) {
+        book.classList.add("read");
+    }
     bookContainer.appendChild(book);
 
+    book.addEventListener("click", () => {
+        this.toggleReadStatus(book);
+    });
+    
     const bookCover = document.createElement("div");
     bookCover.classList.add("bookCover");
     book.appendChild(bookCover);
-
+    
     const bookInfo = document.createElement("div");
     bookInfo.classList.add("bookInfo");
     bookCover.appendChild(bookInfo);
-
+    
     const title = document.createElement("p");
     title.classList.add("title");
-    title.textContent = bookObj.title;
+    title.textContent = this.title;
     bookInfo.appendChild(title);
-
+    
     const author = document.createElement("p");
     author.classList.add("author");
-    author.textContent = bookObj.author;
+    author.textContent = this.author;
     bookInfo.appendChild(author);
-
+    
     const pages = document.createElement("p");
     pages.classList.add("pages");
-    pages.textContent = bookObj.pages;
+    pages.textContent = this.pages;
     bookCover.appendChild(pages);
-
+    
     const removeBook = document.createElement("button");
     removeBook.classList.add("removeBook");
     removeBook.setAttribute("type", "button");
     removeBook.textContent = "Remove Book";
+
+    removeBook.addEventListener("click", () => {
+        bookshelf.removeChild(bookContainer);
+    });
     bookContainer.appendChild(removeBook);
-    
-    const readStatus = document.createElement("input");
-    readStatus.classList.add("readStatus");
-    readStatus.setAttribute("type", "checkbox");
-    bookContainer.appendChild(readStatus);
 
-    bookshelf.appendChild(bookContainer)
-    
-}
+    bookshelf.appendChild(bookContainer);
+};
 
-const harryPotter = new Book("Harry Potter", "J.K.Rowling", "589", true)
+Book.prototype.toggleReadStatus = function(bookElement) {
+    this.readStatus = !this.readStatus;
+    bookElement.classList.toggle("read");
+};
 
-const book = {title: "Harry Potter", author: "J.K.Rowling", pages: "589", readStatus: true}
+const openBtn = document.querySelector("#openBtn");
+const dialog = document.querySelector("dialog");
+openBtn.addEventListener("click", () => {
+    dialog.showModal();
+});
 
-displayBook(harryPotter)
+const submitBtn = dialog.querySelector("#submitBtn");
+const bookInput = dialog.querySelector("#bookInput");
+const authorInput = dialog.querySelector("#authorInput");
+const pageInput = dialog.querySelector("#pageInput");
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const newBook = new Book(bookInput.value, authorInput.value, pageInput.value, false);
+    myLibrary.unshift(newBook);
+
+    newBook.displayBook();
+
+    bookInput.value = "";
+    authorInput.value = "";
+    pageInput.value = "";
+
+    dialog.close();
+});
+
+const cancelBtn = dialog.querySelector("#cancelBtn");
+cancelBtn.addEventListener("click", () => {
+    dialog.close();
+});
